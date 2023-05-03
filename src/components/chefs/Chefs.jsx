88829db33b9a72a703032295322b90e8c7b03fd9
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2023-05-03 20:13:14 +0600

    cardsconsist of six chefs info added

---
+++ src/components/chefs/Chefs.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import './Chefs.css';
+import { Button } from 'react-bootstrap';
+const Chefs = () => {
+    const [chefs, setChefs] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/chefs')
+            .then(res => res.json())
+            .then(data => setChefs(data))
+            .catch(error => console.log(error))
+    }, [])
+
+    console.log(chefs);
+    
+
+    return (
+        <div>
+            <h1 className='text-center fw-bold mt-2 text-danger'>Our Chefs</h1>
+          <Row xs={1} md={2} className="g-4 mt-5 shadow">
+      {chefs.map((chef) => (
+        <Col>
+          <Card className='h-100 shadow'>
+            <Card.Img variant="top" className='img-circle' src={chef.image_url}  />
+            <Card.Body className='chef shadow'>
+              <Card.Title>{chef.name}</Card.Title>
+              <Card.Text>
+             <ul>
+                <li>Years of Experience:{chef.years_of_experience}</li>
+                <li>Numbers of Recipes:{chef.number_of_recipes}</li>
+                <li>Likes:{chef.number_of_likes}</li>
+             </ul>
+                <div>
+                <Button className='bg-danger w-25 text-white fw-semibold border-0 mt-5' >View Recipes</Button>
+                </div>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+        
+            
+        
+       
+            
+        </div>
+    );
+};
+
+export default Chefs;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import Banner from '../components/Banner/Banner';
+import Chefs from '../components/chefs/Chefs';
 
 const Main = () => {
     return (
         <div className='container'>
             <Header></Header>
             <Banner></Banner>
+            <Chefs></Chefs>
             <h2>This is main content</h2>
         </div>
     );
