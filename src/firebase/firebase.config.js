b3815b866c0ea825066ad6baeac55c5cbbe0a7d4
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2023-05-04 23:27:41 +0600

    env varibale set

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('emv', import.meta.env.VITE_PASS);
 const firebaseConfig = {
-  apiKey: "AIzaSyDMyYJRKFoOcWRNC6z5xGt3t12lc-E6UHc",
-  authDomain: "banglar-canteen-254f0.firebaseapp.com",
-  projectId: "banglar-canteen-254f0",
-  storageBucket: "banglar-canteen-254f0.appspot.com",
-  messagingSenderId: "951103755139",
-  appId: "1:951103755139:web:5d0611e4d498b734625cb6"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
