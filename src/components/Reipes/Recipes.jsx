8abe364cf9a2adf53754831dd3f1f0310f26f99f
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2023-05-04 10:28:12 +0600

    recipe page's tabular section which is consist of a table containing recipe info about each chef

--- src/components/Reipes/Recipes.jsx
+++ src/components/Reipes/Recipes.jsx
@@ -3,11 +3,10 @@ import { useLoaderData } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import { Button } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
 import Header from '../Header/Header';
 
 const Recipes = () => {
-    console.log('hello')
     const recipes = useLoaderData();
     console.log(recipes[0].id);
     const [chefs, setChefs] = useState([]);
@@ -18,22 +17,23 @@ const Recipes = () => {
             .catch(error => console.log(error))
     }, [])
     const seletedChef = chefs.find(n => n.id == recipes[0].id);
+    console.log(recipes);
 
     return (
 
         <div>
             <Header></Header>
-           
+
             <div className='d-flex justify-content-between mt-4 container bg-danger'>
                 <div className='d-flex flex-column fs-7 text-start text-wrap '>
                     <p className='d-flex flex-column justify-content-center align-items-center fs-7 text-start text-wrap mt-4 pt-5 ms-0 ps-0 '>
-                    <h3 className='text-start'>{seletedChef?.name}</h3>
-                    {seletedChef?.bio}
+                        <h3 className='text-start'>{seletedChef?.name}</h3>
+                        {seletedChef?.bio}
 
-                    {seletedChef?.name} has {seletedChef?.years_of_experience} years of experience. The numbers of like in the social media is {seletedChef?.number_of_likes}. The number of recipes the chef tried so far is {seletedChef?.number_of_recipes}
+                        {seletedChef?.name} has {seletedChef?.years_of_experience} years of experience. The numbers of like in the social media is {seletedChef?.number_of_likes}. The number of recipes the chef tried so far is {seletedChef?.number_of_recipes}
 
                     </p>
-                    <Button className='bg-danger text-white fw-semibold border-0' >See Menu</Button>
+
 
 
 
@@ -41,6 +41,54 @@ const Recipes = () => {
 
 
                 <img className='ms-4 px-4 img img-fluid w-50' src={seletedChef?.image_trasnparent} alt="" />
+
+            </div>
+            <div className='container mt-5'>
+                <h2>Best Recipe Info</h2>
+                <p>{seletedChef?.name} is excel in many recipes. Some of them are given below:</p>
+                <Table className='bg-info' striped bordered hover>
+                    <thead>
+                        <tr>
+
+                            <th>Recipe Name</th>
+                            <th>Ingredients</th>
+                            <th>Cooking Method</th>
+                            <th>Rating</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+
+                        {
+                            recipes.map((recipe) => (
+                                <>
+                                    <tr>
+                                        <td>{recipe?.recipe_name}</td>
+                                        <td>
+                                            <ol>
+                                               
+
+                                                {recipe?.ingredients.map(name => (
+                                                    <li>
+                                                        {name}
+                                                    </li>
+                                                ))}
+                                            </ol>
+                                        </td>
+                                        <td>{recipe?.cooking_method}</td>
+                                        <td>{recipe?.rating}</td>
+                                    </tr>
+
+                                </>
+
+                            ))
+                        }
+
+                    </tbody>
+
+
+                </Table>
+
 
             </div>
         </div>
